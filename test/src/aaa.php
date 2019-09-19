3722dcb81d8d84c7677980688dae33ed8af7e1b4
吴问应 <wenying.wu@baisonmail.com>
2019-09-19 16:41:12 +0800

    1

---
+++ test/src/aaa.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/19
+ * Time: 16:27
+ */
+namespace Go;
+
+class aaa{
+    public function get(){
+        echo "aaag";
+    }
+}
